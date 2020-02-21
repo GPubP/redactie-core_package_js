21ecae73a976bc080b68b89705d82910ab01e968
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-02-21 17:12:19 +0100

    feat: add support for path prefix

--- public/core/index.ts
+++ public/core/index.ts
@@ -1,7 +1,13 @@
 import Modules from '../modules/modules.class';
 import Routes from '../routes/routes.class';
+import { CoreOptions } from './core.types';
 
-export default {
-	modules: new Modules(),
-	routes: new Routes(),
-};
+export default class Core {
+	public modules: Modules;
+	public routes: Routes;
+
+	constructor({ pathPrefix }: CoreOptions) {
+		this.modules = new Modules();
+		this.routes = new Routes(pathPrefix);
+	}
+}
--- public/routes/routes.class.test.tsx
+++ public/routes/routes.class.test.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { FC } from 'react';
 import Routes from './routes.class';
 import { ModuleRouteConfig } from './routes.types';
+
+const DummyComponent: FC = () => <div>test</div>;
 
 describe('Routes', () => {
 	let routes: Routes;
 	const dummyRouteConfig: ModuleRouteConfig = {
 		path: '/external',
-		component: () => <div>test</div>,
-	}
+		component: DummyComponent,
+		routes: [
+			{
+				path: '/external/child',
+				component: DummyComponent,
+			},
+		],
+	};
 
 	beforeEach(() => {
-		routes = new Routes()
-	})
+		routes = new Routes();
+	});
 
 	it('Should be able to register a route', () => {
 		expect(routes).toBeInstanceOf(Routes);
 		routes.register(dummyRouteConfig);
 		expect(routes.getAll()).toEqual([dummyRouteConfig]);
-	})
+	});
 
 	it('Should be able to render all routes', () => {
 		expect(routes).toBeInstanceOf(Routes);
 		routes.register(dummyRouteConfig);
 		const allRoutes = routes.getAll();
-		expect(routes.render(allRoutes)).toBeInstanceOf(Object)
-	})
-})
+		expect(routes.render(allRoutes)).toBeInstanceOf(Object);
+	});
+
+	it('Should set pathPrefix recursively when available', () => {
+		const routesWithPrefix = new Routes('/prefix');
+		routesWithPrefix.register(dummyRouteConfig);
+
+		const allRoutes = routesWithPrefix.getAll();
+
+		expect(allRoutes[0].path.includes('/prefix')).toBeTruthy();
+		expect(allRoutes[0].routes && allRoutes[0].routes[0].path.includes('/prefix')).toBeTruthy();
+
+	});
+});
