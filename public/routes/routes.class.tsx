43aea8c092a7500e71347ebc09f6110a97d287f8
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-02-22 09:22:38 +0100

    feat: create observable pattern

--- public/routes/routes.class.tsx
+++ public/routes/routes.class.tsx
@@ -1,27 +1,58 @@
 import React, { ReactElement } from 'react';
+import { Subject, Observable } from 'rxjs';
 import { Route, RouteComponentProps, Switch, SwitchProps } from 'react-router-dom';
-import { ModuleRouteConfig } from './routes.types';
+import { ModuleRouteConfig, ChildModuleRouteConfig, RouteOptions } from './routes.types';
 
 export default class Routes {
 	private pathPrefix = '';
 	private registeredRoutes: ModuleRouteConfig[] = [];
+	private registeredRoutesSubject: Subject<ModuleRouteConfig[]> = new Subject();
+
+	public routesChanges: Observable<ModuleRouteConfig[]> = this.registeredRoutesSubject.asObservable();
 
 	public setPathPrefix(pathPrefix = ''): void {
 		this.pathPrefix = pathPrefix;
 	}
 
-	public register(routeConfig: ModuleRouteConfig): void {
-		const newRoute = routeConfig;
+	public register(routeConfig: ModuleRouteConfig, options: RouteOptions = { prefix: true }): void {
 		const defaultRouteExists = this.registeredRoutes.find(
 			(route: ModuleRouteConfig) => route.isDefaultRoute
 		);
 
-		if (defaultRouteExists) {
+		if (defaultRouteExists && routeConfig.isDefaultRoute) {
 			console.warn('Default route already exists.');
-			newRoute.isDefaultRoute = false;
 		}
 
-		this.registeredRoutes.push(this.prefixRoute(newRoute));
+		const newRouteConfig = {
+			...options.prefix ? this.prefixRoute(routeConfig) : routeConfig,
+			isDefaultRoute: defaultRouteExists ? false : routeConfig.isDefaultRoute,
+		};
+
+		this.registeredRoutes.push(newRouteConfig);
+		this.registeredRoutesSubject.next(this.registeredRoutes);
+	}
+
+	public updateChildRoutes(path: string, routes: ChildModuleRouteConfig[], options: RouteOptions = { prefix: true }): void {
+		this.registeredRoutes = this.registeredRoutes.map((route) => {
+			if (route.path === this.getRoutePath(path)) {
+				return {
+					...route,
+					routes: options.prefix ? routes.map((r) => this.prefixRoute(r)) : routes,
+				};
+			}
+			return route;
+		});
+		this.registeredRoutesSubject.next(this.registeredRoutes);
+	}
+
+	public update(path: string, routeConfig: ModuleRouteConfig, options: RouteOptions = { prefix: true }): void {
+		this.registeredRoutes = this.registeredRoutes.map((route) => {
+			if (route.path === this.getRoutePath(path)) {
+				return options.prefix ? this.prefixRoute(routeConfig) : routeConfig;
+			}
+			return route;
+		});
+		this.registeredRoutesSubject.next(this.registeredRoutes);
 	}
 
 	public getAll(): ModuleRouteConfig[] {
@@ -61,5 +92,9 @@ export default class Routes {
 			path: `${this.pathPrefix}${routeConfig.path}`,
 			routes: routeConfig.routes && routeConfig.routes.map((route) => this.prefixRoute(route)),
 		};
+	}
+
+	private getRoutePath(path: string): string {
+		return `${this.pathPrefix}${path}`;
 	}
 }
