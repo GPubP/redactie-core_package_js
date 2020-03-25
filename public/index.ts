9a9d15b957cb09a161dde09f4788efb7790e1870
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-03-25 15:42:40 +0100

    feat: export route config component prop types

--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,10 @@
 import Core from './core';
 
 export { ModuleAPI } from './modules/modules.types';
-export { ModuleRouteConfig } from './routes/routes.types';
+export {
+	ChildRouteConfigComponentProps,
+	ModuleRouteConfig,
+	RouteConfigComponentProps,
+} from './routes/routes.types';
 
 export default Core;
