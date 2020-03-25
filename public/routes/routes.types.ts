f4c8de902e07892029c2d8833ac9a24b6f077fe1
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-03-25 15:42:00 +0100

    fix(routes): change component type props to any

--- public/routes/routes.types.ts
+++ public/routes/routes.types.ts
@@ -1,40 +1,43 @@
+import { ComponentType } from 'react';
 import { RouteComponentProps, SwitchProps } from 'react-router-dom';
 import { Observable } from 'rxjs';
 
-export interface RouteConfigComponentProps<Params extends { [K in keyof Params]?: string } = {}> extends RouteComponentProps<Params> {
-	route?: ModuleRouteConfig;
-}
-
-export interface ChildRouteConfigComponentProps<Params extends { [K in keyof Params]?: string } = {}> extends RouteComponentProps<Params> {
-	route?: ChildModuleRouteConfig;
-}
-
-export interface BaseRouteConfig {
-	key?: string;
-	label?: string;
-	path: string;
-	routes?: ChildModuleRouteConfig[];
+interface RouteExtraProps {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	[propName: string]: any;
 }
 
+export interface RouteConfigComponentProps<Params extends { [K in keyof Params]?: string } = {}> extends RouteComponentProps<Params> {
+	route: ModuleRouteConfig;
+}
+
+export interface ChildRouteConfigComponentProps<Params extends { [K in keyof Params]?: string } = {}> extends RouteComponentProps<Params> {
+	route: ChildModuleRouteConfig;
+}
+
+export interface BaseRouteConfig extends RouteExtraProps {
+	key?: string;
+	label?: string;
+	path: string;
+	routes?: ChildModuleRouteConfig[];
+}
+
 export interface ModuleRouteConfig extends BaseRouteConfig {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	component: React.ComponentType<RouteConfigComponentProps<any>> | React.ComponentType | React.ComponentType<{ route: ModuleRouteConfig }>;
+	component: ComponentType<any>;
 	isDefaultRoute?: boolean;
 }
 
 export interface ChildModuleRouteConfig extends BaseRouteConfig {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	component: React.ComponentType<RouteConfigComponentProps<any>> | React.ComponentType | React.ComponentType<{ route: ChildModuleRouteConfig }>;
+	component: ComponentType<any>;
 }
 
 export interface Routes {
 	register: (routeConfig: ModuleRouteConfig) => void;
 	getAll: () => ModuleRouteConfig[];
 	routesChanges: Observable<ModuleRouteConfig[] | null>;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	render: (routeConfig: ModuleRouteConfig[], extraProps?: any, switchProps?: SwitchProps) => object;
+	render: (routeConfig: ModuleRouteConfig[], extraProps?: RouteExtraProps, switchProps?: SwitchProps) => object;
 	setPathPrefix: (prefix: string) => void;
 }
 
