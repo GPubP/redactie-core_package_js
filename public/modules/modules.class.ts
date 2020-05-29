1064ea93bd75c7052af4708bcd8eb76dcc8fc9d3
Glenn Verschooren <Glenn.Verschooren@digipolis.be>
2020-05-29 16:54:19 +0200

    feat: add selectModuleAPI function

--- public/modules/modules.class.ts
+++ public/modules/modules.class.ts
@@ -1,18 +1,32 @@
-import { ModuleAPI } from './modules.types';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+import { ModuleAPI, ModuleAPIMap } from './modules.types';
 
 export default class Modules {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	private registeredModules: { [key: string]: any } = {};
+	private registeredModules: ModuleAPIMap = {};
+	private registeredModulesSubject = new BehaviorSubject<ModuleAPIMap>({});
+
+	public registeredModules$ = this.registeredModulesSubject.asObservable();
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	exposeModuleApi(moduleName: string, api: { [key: string]: any }): void {
+	public exposeModuleApi(moduleName: string, api: ModuleAPI): void {
 		if (this.registeredModules[moduleName]) {
 			throw 'Module already exists';
 		}
 
 		this.registeredModules[moduleName] = api;
+		this.registeredModulesSubject.next(this.registeredModules);
 	}
-	getModuleAPI(moduleName: string): ModuleAPI {
+
+	public getModuleAPI(moduleName: string): ModuleAPI {
 		return this.registeredModules[moduleName];
+	}
+
+	public selectModuleAPI(moduleName: string): Observable<ModuleAPI> {
+		return this.registeredModules$.pipe(
+			filter((modules) => modules[moduleName]),
+			map((modules) => modules[moduleName])
+		);
 	}
 }
--- public/modules/modules.types.ts
+++ public/modules/modules.types.ts
@@ -2,3 +2,8 @@ export interface ModuleAPI {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	[key: string]: any;
 }
+
+export interface ModuleAPIMap {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	[key: string]: any;
+}
